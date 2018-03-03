8a342695ef461e9a10a55abd4c38831ac55cdbbf
Alex Satsunkevich <alexander.satsunkevich@gmail.com>
2018-03-03 23:40:40 +0300

    It is a cope of 'user' aplication. Added only for training

---
+++ task_DEV-1/TaskDev1/EqualSymbolsCounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TaskDev1
+{
+    /// <summary>
+    /// This class works with string and
+    /// count max lenght of equal symbols.
+    /// </summary>
+    class EqualSymbolsCounter
+    {
+        public EqualSymbolsCounter (string ourString)
+        {
+            currentString = ourString;
+        }
+        private string currentString;
+        /// <summary>
+        /// This method finds max lenght of idential symbols.
+        /// </summary>
+        /// <returns>Returns the maximom number of dublicate succsessive symbols.</returns>
+        public int FindSimilarSimbolsSequanceLength()
+        {
+            int maxSameStringLenght = 1;
+            int currentSequenceLength = 1;
+            for (int i = 0; i < currentString.Length - 1; i++)
+            {
+                if (currentString[i] == currentString[i + 1])
+                {
+                    currentSequenceLength++;
+                }
+                else
+                {
+                    currentSequenceLength = 1;
+                }
+                if (maxSameStringLenght < currentSequenceLength)
+                {
+                    maxSameStringLenght = currentSequenceLength;
+                }
+            }
+            return maxSameStringLenght;
+        }
+    }
+}
