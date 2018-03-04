98e71181b64f09bbbe7c34099a9ce534e216c83e
Alex Satsunkevich <alexander.satsunkevich@gmail.com>
2018-03-04 15:23:59 +0300

    task_DEV-2

---
+++ task_DEV-2/task_DEV-2/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace task_DEV_2
+{
+    /// <summary>
+    /// Entry point in programm
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            EvenCharacters sequence = new EvenCharacters();
+            sequence.EnterSequance();
+            sequence.WriteEvenCharachters();
+        }
+    }
+}
---
+++ task_DEV-2/task_DEV-2/EvenCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_2
+{
+    /// <summary>
+    /// This class for work with sequances
+    /// </summary>
+    class EvenCharacters
+    {
+        /// <summary>
+        /// Enter Sequance
+        /// </summary>
+        public void EnterSequance ()
+        {
+            currentString = Console.ReadLine();
+            StringBuilder CharacterSequence = new StringBuilder(currentString);
+        }
+        private string currentString;
+        /// <summary>
+        /// This method displays even-numbered charachters
+        /// </summary>
+        public void WriteEvenCharachters ()
+        {
+            for (int i = 1; i < currentString.Length; i = i + 2)
+            {
+                Console.Write(currentString[i]);
+            }
+        }
+    }
+}
