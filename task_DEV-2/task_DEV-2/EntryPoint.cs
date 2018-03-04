74216c26fd9cbb1ad09a5c9df645a219edcca3ac
Alex Satsunkevich <alexander.satsunkevich@gmail.com>
2018-03-04 19:44:09 +0300

    task_DEV-2

---
+++ task_DEV-2/task_DEV-2/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace task_DEV_2
+{
+    /// <summary>
+    /// Entry point in programm
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            StringCreater sequence = new StringCreater();
+            sequence.EnterSequance();
+            sequence.WriteEvenCharacters();
+        }
+    }
+}
