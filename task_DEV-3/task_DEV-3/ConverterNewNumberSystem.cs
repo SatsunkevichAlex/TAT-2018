1607f9479c3512e8c581693aae13e874fdb39a69
Alex Satsunkevich <alexander.satsunkevich@gmail.com>
2018-03-07 00:00:45 +0300

    task_DEV-3

---
+++ task_DEV-3/task_DEV-3/ConverterNewNumberSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_DEV_3
+{
+    /// <summary>
+    /// This is a class for convert integer numbers to a new scale of notation
+    /// </summary>
+    class ConverterNewNumberSystem
+    {
+        /// <summary>
+        /// This method convert convert integer numbers to a new scale of notation
+        /// and display it
+        /// </summary>
+        /// <param name="NumberToChange"></param>
+        /// <param name="SystemBase"></param>
+        public void ConverterToNewNumberSystem(int NumberToChange, int SystemBase)
+        {
+            string ReversedNewNumber = "";
+            int NumberOfNewNumeral;
+            while (NumberToChange != 0)
+            {
+                NumberOfNewNumeral = NumberToChange % SystemBase;
+                NumberToChange /= SystemBase;
+                ReversedNewNumber += Convert.ToString(NumberOfNewNumeral);
+            }
+            for (int i = ReversedNewNumber.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{ReversedNewNumber[i]}");
+            }
+            
+        }
+    }
+}
---
+++ task_DEV-3/task_DEV-3/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_DEV_3
+{
+    /// <summary>
+    /// Entry point in programm
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber;
+            int baseOfNewSystem;
+            bool CheckNumber = Int32.TryParse(args[0], out inputNumber);
+            bool CheckFormatBase = Int32.TryParse(args[1], out baseOfNewSystem);
+            bool CheckIntervalBase = baseOfNewSystem >= 2 && baseOfNewSystem <= 20;
+            if (CheckNumber && CheckFormatBase && CheckIntervalBase)
+            {
+                ConverterNewNumberSystem converter = new ConverterNewNumberSystem();
+                converter.ConverterToNewNumberSystem(inputNumber, baseOfNewSystem);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
