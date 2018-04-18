fe4b075a245a06c24fc59aed43db5c7f178c0597
Alex Satsunkevich <alexander.satsunkevich@gmail.com>
2018-04-18 16:06:56 +0300

    task_DEV-8

---
+++ task_DEV-8/task_DEV-8/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_8
+{
+    /// <summary>
+    /// Interface which provides command
+    /// </summary>
+    interface ICommand
+    {
+        /// <summary>
+        /// Execute command
+        /// </summary>
+        void Execute();
+    }
+}
---
+++ task_DEV-8/task_DEV-8/InformationInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_8
+{
+    /// <summary>
+    /// Class command for input information about user 
+    /// </summary>
+    class InformationInput : ICommand
+    {
+        User user;
+        public InformationInput(User u)
+        {
+            user = u;
+        }
+
+        /// <summary>
+        /// Execute first name input command
+        /// </summary>
+        public void Execute()
+        {
+            user.InformationInput();
+        }
+    }
+}
