543aa62a08c979584028743e9a201aa4baa57371
Alex <sasha.satsunkevich@gmail.com>
2018-05-07 02:25:09 +0300

    Update

--- task_DEV-9/task_DEV-9/User.cs
+++ task_DEV-9/task_DEV-9/User.cs
@@ -7,7 +7,7 @@ namespace task_DEV_9
     /// </summary>
     public class User
     {
-        public string Email { get; set; } = "+375291662667"; // I changed user Email and Password before push it on github. Program work correctly.
-        public string Password { get; set; } = "asaev1997";
+        public string Email { get; set; } = "Correct email."; // I changed user Email and Password before push it on github. Program work correctly.
+        public string Password { get; set; } = "Correct password";
     }
 }
