95ba1143c92b3c74c7fe3fefad84800fd0602c7b
Alex <sasha.satsunkevich@gmail.com>
2018-05-09 23:08:58 +0300

    Add task_DEV-9(av.by)

---
+++ Classwork(02.05.2018)/Classwork9(02.05.2018)/Locators/SearchResultPageLocators.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Classwork9_02._05._2018_.Locators
+{
+    /// <summary>
+    /// Search result page locators.
+    /// </summary>
+    public class SearchResultPageLocators
+    {
+        public By sortByPriceAscending = By.XPath("//li[a[2]]");
+        public By cheapestBMW_M5 = By.XPath("//div[@class = 'listing-item-price'][1]//strong");
+    }
+}
