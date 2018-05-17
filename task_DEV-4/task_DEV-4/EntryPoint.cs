c1ec3b257dd06d9d0b3ee608a7cc72fbf26d9c0b
Alex <sasha.satsunkevich@gmail.com>
2018-05-17 22:10:52 +0300

    Add task_DEV-4

---
+++ task_DEV-4/task_DEV-4/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_DEV_4
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Alex\source\repos\TAT-2018\task_DEV-4\task_DEV-4\Persons.xml";
+            try
+            {
+                ParserForXmlFile str = new ParserForXmlFile(path);
+                str.OutputingDataToconsole();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
